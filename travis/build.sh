72cb4189da7c3392cd162851c67c3e94e0e38899
Chris Johnston <chjohnston@protonmail.com>
2019-02-13 01:47:19 -0800

    try splitting up the build and deploy scripts

--- travis/build.sh
+++ travis/build.sh
@@ -2,7 +2,6 @@
 # Build script for Travis CI
 # based on https://savaslabs.com/2016/10/25/deploy-jekyll-with-travis.html
 
-
 # !!! THIS MAY BE DESTRUCTIVE IF RUN IN YOUR DEV ENVIRONMENT !!!
 
 echo "Running Travis CI build script."
@@ -13,27 +12,6 @@ then
     set -e
     npm install
     gulp
-
-    # checkout gh-pages branch and remove all existing files
-    # into the directory ../gh_pages
-    git clone --branch gh-pages https://${GH_TOKEN}@github.com/Chris-Johnston/uwbacm-site.git ../gh_pages
-    cd ../gh_pages
-    git checkout gh-pages
-    rm -rf *
-
-    # copy the built files from the original branch into the output
-    cd -R ../uwbacm-site/* .
-    cp ../uwbacm-site/.travis.yml .
-
-    git config user.email ${GH_EMAIL}
-    git config user.name "Travis CI / UWB-ACM"
-
-    # commit this to the gh-pages branch
-    git status
-    git add -A .
-    git status
-    git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-    git push --quiet origin gh-pages
 else
     echo "This script should not be run outside of Travis CI!"
 fi
