d5c9ed64b5d33e369482c36de63175b1f7c5dae2
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-04-29 15:47:42 -0700

    re-implement changing words functionality in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import AcmLargeLogo from '../images/logos/acm/acm-large-logo.jpg';
 import AcmTransparentLogo from '../images/logos/acm/acm-white-transparent.svg';
+
+const topWords = ["UWB ACM", "visionaries", "diverse", "proactive"];
+
+const ChangingWord = () => {
+	const [curWordIndex, setWordIndex] = useState(0);
+	const [wordCounter, setWordCounter] = useState(0);
+ 	useEffect(() => {
+		let timer = setTimeout(() => {
+			if(wordCounter < topWords[curWordIndex].length) {
+				setWordCounter(wordCounter + 1);
+			}
+			else {
+				setWordIndex(curWordIndex + 1 < topWords.length ? curWordIndex + 1 : 0);
+				setWordCounter(0);
+			}
+		}, 500);
+		return () => {
+			clearTimeout(timer);
+		}
+	});
+	return <span id="changing-word">{topWords[curWordIndex].slice(0, wordCounter)}</span>
+}
 
 const Header = () => (
 	<div id="top-fold" className="center-div">
 		<div id="top-fold__center">
 			<h1>
-				We're <span id="changing-word">UWB ACM</span> <span className="flashing">|</span>
+				We're <ChangingWord /> <span className="flashing">|</span>
 			</h1>
 			<h2>and we prepare students for tomorrow's tech challenges.</h2>
 		</div>
