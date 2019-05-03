4b18e141af1f23b0abf8f311b789792d37b8926b
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-03 13:45:20 -0700

    remove commented out code
    this code isn't needed anymore.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -103,64 +103,6 @@ const Signup = () => {
 						</Form>
 					)}
 				</Formik>
-				{/* TODO: Re-implement using Formik */}
-				{/* Begin Mailchimp Signup htmlForm */}
-				{/* <link href="//cdn-images.mailchimp.com/embedcode/classNameic-10_7.css" rel="stylesheet" type="text/css" />
-		<div id="mc_embed_signup">
-			<form
-				action="https://uwbacm.us16.list-manage.com/subscribe/post?u=8773dffc3f528b0b18fd7aef8&id=11b31196c4"
-				method="post"
-				id="mc-embedded-subscribe-htmlForm"
-				name="mc-embedded-subscribe-htmlForm"
-				className="validate"
-				target="_blank"
-        noValidate
-        onChange={() => {}}
-			>
-				<div id="mc_embed_signup_scroll">
-					<h2>Subscribe to our mailing list</h2>
-					<div className="indicates-required">
-						<span className="asterisk">*</span> indicates required
-					</div>
-					<div className="mc-field-group">
-						<label htmlFor="mce-EMAIL">
-							Email Address <span className="asterisk">*</span>
-						</label>
-						<input type="email" value="" name="EMAIL" className="required email" id="mce-EMAIL" />
-					</div>
-					<div className="mc-field-group">
-						<label htmlFor="mce-FNAME">
-							First Name <span className="asterisk">*</span>
-						</label>
-						<input type="text" value="" name="FNAME" className="required" id="mce-FNAME" />
-					</div>
-					<div className="mc-field-group">
-						<label htmlFor="mce-LNAME">
-							Last Name <span className="asterisk">*</span>
-						</label>
-						<input type="text" value="" name="LNAME" className="required" id="mce-LNAME" />
-					</div>
-					<div id="mce-responses" className="clear">
-						<div className="response" id="mce-error-response" style={{display: 'none'}} />
-						<div className="response" id="mce-success-response" style={{display: 'none'}} />
-					</div>{' '} */}
-				{/* real people should not fill this in and expect good things - do not remove this or risk htmlForm bot signups */}
-				{/* <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
-						<input type="text" name="b_8773dffc3f528b0b18fd7aef8_11b31196c4" tabIndex="-1" value="" />
-					</div>
-					<div className="clear">
-						<input
-							type="submit"
-							value="Subscribe"
-							name="subscribe"
-							id="mc-embedded-subscribe"
-							className="button"
-						/>
-					</div>
-				</div>
-			</form>
-		</div> */}
-				{/* End mc_embed_signup */}
 			</Row>
 			<Row>{hasSubmitted && <Alert variant="success">The form has been submitted!</Alert>}</Row>
 		</Container>
