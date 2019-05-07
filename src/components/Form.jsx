83aff18127410e94781e4f3db7d926d6850762a1
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-07 15:40:32 -0700

    Update Form to closer resembled original styling
    Also removes the need of bootstrap for styling of the Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,27 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import React, {useState} from 'react';
 
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Alert from 'react-bootstrap/Alert';
-
 import * as Yup from 'yup';
-import {Formik} from 'formik';
+import {Formik, Field} from 'formik';
 
 const SignUpSchema = Yup.object().shape({
-	email: Yup.string()
+	EMAIL: Yup.string()
 		.email('Invalid email address')
 		.required('Required'),
-	firstName: Yup.string()
+	FNAME: Yup.string()
 		.min(2, 'Must be longer than 2 characters')
 		.max(20, 'Nice try, nobody has a first name that long')
 		.required('Required'),
-	lastName: Yup.string()
+	LNAME: Yup.string()
 		.min(2, 'Must be longer than 2 characters')
 		.max(20, 'Nice try, nobody has a last name that long')
 		.required('Required')
@@ -29,83 +19,92 @@ const SignUpSchema = Yup.object().shape({
 
 const Signup = () => {
 	const [hasSubmitted, hasSubmittedHandler] = useState(false);
-
 	return (
-		<Container id="join-acm">
-			<Row>
-				<h2>Subscribe to our mailing list</h2>
-			</Row>
-			<Row>
-				<Formik
-					initialValues={{
-						email: 'email@email.com',
-						firstName: 'FirstName',
-						lastName: 'LastName'
-					}}
-					validationSchema={SignUpSchema}
-					onSubmit={(values) => {
-						/* Post to the survey form */
-						fetch('https://uwbacm.us16.list-manage.com/subscribe/post?u=8773dffc3f528b0b18fd7aef8&id=11b31196c4', {
-							method: 'post',
-							body: values,
-							mode: 'no-cors'
-						}).then((response) => {
-							hasSubmittedHandler(true);
-						});
-					}}
-				>
-					{({handleSubmit, handleChange, handleBlur, values, touched, isValid, errors}) => (
-						<Form noValidate onSubmit={handleSubmit}>
-							<Form.Row>
-								<Form.Group as={Col} md="3" controlId="email">
-									<Form.Label>Email</Form.Label>
-									<InputGroup>
-										<Form.Control
-											type="text"
-											placeholder="test@email.com"
-											name="email"
-											value={values.email}
-											onChange={handleChange}
-											isInvalid={!!errors.email}
-										/>
-										<Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
-									</InputGroup>
-								</Form.Group>
-								<Form.Group as={Col} md="3" controlId="firstName">
-									<Form.Label>First name</Form.Label>
-									<Form.Control
-										type="text"
-										name="firstName"
-										value={values.firstName}
-										onChange={handleChange}
-										isValid={touched.firstName && !errors.firstName}
+		<div id="join-acm" className="center-div">
+			<link href="https://cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css" />
+			<Formik
+				validateOnChange
+				initialValues={{
+					EMAIL: 'email@email.com',
+					FNAME: 'FirstName',
+					LNAME: 'LastName'
+				}}
+				validationSchema={SignUpSchema}
+				onSubmit={(values) => {
+					/* Post to the survey form */
+					fetch('https://uwbacm.us16.list-manage.com/subscribe/post?u=8773dffc3f528b0b18fd7aef8&id=11b31196c4', {
+						method: 'post',
+						body: values,
+						mode: 'no-cors'
+					}).then((response) => {
+						hasSubmittedHandler(true);
+					});
+				}}
+			>
+				{({handleSubmit, handleChange, handleBlur, values, touched, isValid, errors}) => (
+					<div id="mc_embed_signup">
+						<form
+							id="mc-embedded-subscribe-form"
+							name="mc-embedded-subscribe-form"
+							className="validate"
+							target="_blank"
+							noValidate
+							onSubmit={handleSubmit}
+						>
+							<div id="mc_embed_signup_scroll">
+								<h2>Subscribe</h2>
+								<div className="indicates-required">
+									<span className="asterisk">*</span>indicates required
+								</div>
+								<div className="mc-field-group">
+									<label htmlFor="mce-EMAIL">
+										Email Address
+										<span className="asterisk">*</span>
+									</label>
+									<Field type="email" name="EMAIL" className="required email" id="mce-EMAIL" />
+								</div>
+								<div className="mc-field-group">
+									<label htmlFor="mce-FNAME">
+										First Name
+										<span className="asterisk">*</span>
+									</label>
+									<Field type="text" name="FNAME" className="required" id="mce-FNAME" />
+								</div>
+								<div className="mc-field-group">
+									<label htmlFor="mce-LNAME">
+										Last Name
+										<span className="asterisk">*</span>
+									</label>
+									<Field type="text" name="LNAME" className="required" id="mce-LNAME" />
+								</div>
+								{/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
+								<div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
+									<input type="text" name="b_8773dffc3f528b0b18fd7aef8_11b31196c4" tabIndex="-1" defaultValue="" />
+								</div>
+								<div className="clear">
+									<input
+										type="submit"
+										defaultValue="Subscribe"
+										name="subscribe"
+										id="mc-embedded-subscribe"
+										className="button"
 									/>
-									<Form.Control.Feedback type="invalid">{errors.firstName}</Form.Control.Feedback>
-								</Form.Group>
-								<Form.Group as={Col} md="3" controlId="lastName">
-									<Form.Label>Last name</Form.Label>
-									<Form.Control
-										type="text"
-										name="lastName"
-										value={values.lastName}
-										onChange={handleChange}
-										isValid={touched.firstName && !errors.lastName}
-									/>
-									<Form.Control.Feedback type="invalid">{errors.lastName}</Form.Control.Feedback>
-								</Form.Group>
-							</Form.Row>
-							{/* real people should not fill this in and expect good things - do not remove this or risk htmlForm bot signups */}
-							<div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
-								<input type="text" name="b_8773dffc3f528b0b18fd7aef8_11b31196c4" tabIndex="-1" value="" readOnly />
+								</div>
+								<div id="mce-responses" className="clear">
+									{errors &&
+										Object.entries(errors).map(([key, val], i) => (
+											<div key={i} className="response">
+												{key}: {val}
+											</div>
+										))}
+									{hasSubmitted && <div className="response">Form has been submitted!</div>}
+								</div>
 							</div>
-
-							<Button type="submit">Submit form</Button>
-						</Form>
-					)}
-				</Formik>
-			</Row>
-			<Row>{hasSubmitted && <Alert variant="success">The form has been submitted!</Alert>}</Row>
-		</Container>
+						</form>
+					</div>
+				)}
+			</Formik>
+		</div>
 	);
 };
 
