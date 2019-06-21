f54b93e72f844277d5c7f76c1e65c955da2f01b1
etcadinfinitum <lizzy.presland@gmail.com>
2019-06-21 16:55:28 -0700

    ensure sentence whitespace in contact section heccin OCD

--- src/pages/home/Contact.jsx
+++ src/pages/home/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => (
 		<div id="contact__center">
 			<h2>Contact</h2>
 			<p>
-				If you have any questions, please feel free to email us at
+				If you have any questions, please feel free to email us at{' '}
 				<a href="mailto:acmuwbothell@gmail.com">acmuwbothell@gmail.com</a>
 			</p>
 		</div>
