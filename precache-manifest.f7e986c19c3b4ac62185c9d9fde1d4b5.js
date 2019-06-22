47ff23570a4807e7db68cdd516f02952f67c5b40
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-22 01:24:05 +0000

    Deploy UWB-ACM/uwbacm-site to github.com/UWB-ACM/uwbacm-site.git:gh-pages

---
+++ precache-manifest.f7e986c19c3b4ac62185c9d9fde1d4b5.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389832e762c1a310129d871fea73a3fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df1e4c8cd6ebee585483",
+    "url": "/static/css/main.31d911e2.chunk.css"
+  },
+  {
+    "revision": "daae57d6183ddf70d548",
+    "url": "/static/js/2.d23e61f2.chunk.js"
+  },
+  {
+    "revision": "df1e4c8cd6ebee585483",
+    "url": "/static/js/main.52e9d182.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb4772e37e30c0a34775d81b2cea4de4",
+    "url": "/static/media/acm-large-logo.bb4772e3.jpg"
+  },
+  {
+    "revision": "28d4ffe823f79a79e98bc0bf0edcad38",
+    "url": "/static/media/acm-white-transparent.28d4ffe8.svg"
+  },
+  {
+    "revision": "dc918dfa5ed94e3b27a72e7d0d9c407d",
+    "url": "/static/media/avi-annie.dc918dfa.jpg"
+  },
+  {
+    "revision": "44e4316b7287397640d11823be48ac41",
+    "url": "/static/media/avi-belle.44e4316b.jpg"
+  },
+  {
+    "revision": "e196747ae68c56e569c87faf4094a4aa",
+    "url": "/static/media/avi-billy.e196747a.jpg"
+  },
+  {
+    "revision": "34aeb6ef41598df2ceed6ce426ac335a",
+    "url": "/static/media/avi-brady.34aeb6ef.jpg"
+  },
+  {
+    "revision": "02b709d347dc16167c7cea3b4e9975f9",
+    "url": "/static/media/avi-chris.02b709d3.jpg"
+  },
+  {
+    "revision": "69cea507c6354d4398f83d126123402b",
+    "url": "/static/media/avi-danielle.69cea507.jpg"
+  },
+  {
+    "revision": "875e22abe87b0816a6465792a23d59f6",
+    "url": "/static/media/avi-delaney.875e22ab.jpg"
+  },
+  {
+    "revision": "6813b4e5be63cb7c8ebe4ec9eccbd155",
+    "url": "/static/media/avi-fahad.6813b4e5.jpg"
+  },
+  {
+    "revision": "adf80a9e5bf58b2f0015b79b607fa73a",
+    "url": "/static/media/avi-gahl.adf80a9e.jpg"
+  },
+  {
+    "revision": "ccd8b03a59b1ede16d0b3b6225d91c9a",
+    "url": "/static/media/avi-hannah.ccd8b03a.jpg"
+  },
+  {
+    "revision": "368f6015b533eb901f79b7cecd307ab9",
+    "url": "/static/media/avi-kash.368f6015.jpg"
+  },
+  {
+    "revision": "8f301f68bd9c27dfd4aeb4b71a9d9d85",
+    "url": "/static/media/avi-lizzy.8f301f68.jpg"
+  },
+  {
+    "revision": "fbfdaa6a2d2396567388ddc0c5313a71",
+    "url": "/static/media/avi-michael.fbfdaa6a.jpg"
+  },
+  {
+    "revision": "11bca716b3c0290c0d988e88e13eeece",
+    "url": "/static/media/avi-michele.11bca716.jpg"
+  },
+  {
+    "revision": "1fbcf949579e961eec5b633e243e6921",
+    "url": "/static/media/avi-prat.1fbcf949.jpg"
+  },
+  {
+    "revision": "9f04646653ca168049d5d7e4541f3dda",
+    "url": "/static/media/avi-saam.9f046466.jpg"
+  },
+  {
+    "revision": "24c2d2090083e18055cd82a4e6b7e73c",
+    "url": "/static/media/avi-sneha.24c2d209.jpg"
+  },
+  {
+    "revision": "34d6ae01c9ece0b1866bf97017dd83de",
+    "url": "/static/media/avi-thomas.34d6ae01.jpg"
+  },
+  {
+    "revision": "55d1fcd634043f441a134705dee3a4dc",
+    "url": "/static/media/avi-xinyu.55d1fcd6.jpg"
+  },
+  {
+    "revision": "c1d86c8637401e51114d99e0e019aa4a",
+    "url": "/static/media/check.c1d86c86.svg"
+  },
+  {
+    "revision": "ca147c586330d253e607c505af7d7f76",
+    "url": "/static/media/contact-background.ca147c58.svg"
+  },
+  {
+    "revision": "4750f66798b0088e9c47960cd5081661",
+    "url": "/static/media/contact.4750f667.svg"
+  },
+  {
+    "revision": "0dab0e9dd478837b0e0bb5bfe5566878",
+    "url": "/static/media/events.0dab0e9d.svg"
+  },
+  {
+    "revision": "593e2633472485bf8cc0ab88cfe7e446",
+    "url": "/static/media/favicon.593e2633.ico"
+  },
+  {
+    "revision": "708c5a7aafcc9064e14e759c586e78c3",
+    "url": "/static/media/github-social.708c5a7a.svg"
+  },
+  {
+    "revision": "28d9ebd97e64fbcfdfdaed9e04abe2e1",
+    "url": "/static/media/hackathon_1.28d9ebd9.jpg"
+  },
+  {
+    "revision": "12a89fae80218327ee6aeeda997434af",
+    "url": "/static/media/hackathon_2.12a89fae.jpg"
+  },
+  {
+    "revision": "28c4f353b76714ca4fc01b47b1113603",
+    "url": "/static/media/hackathon_3.28c4f353.jpg"
+  },
+  {
+    "revision": "6e687ad0d8b79a77c597734130fa6ab6",
+    "url": "/static/media/hackathon_4.6e687ad0.jpg"
+  },
+  {
+    "revision": "6bc0ef25c2cefd5f8eceb2801091a863",
+    "url": "/static/media/hackathon_5.6bc0ef25.jpg"
+  },
+  {
+    "revision": "f7999843e77bee31b1d104ef32b27e38",
+    "url": "/static/media/hackathon_6.f7999843.jpg"
+  },
+  {
+    "revision": "e9c1993e96883b68851b9415fbeb5242",
+    "url": "/static/media/join-acm.e9c1993e.svg"
+  },
+  {
+    "revision": "cf9e3c80eb9d1f1424f80f40ec28c3ad",
+    "url": "/static/media/linkedin-social.cf9e3c80.svg"
+  },
+  {
+    "revision": "6de143c41da7e3f477e0f1e34d7da3c4",
+    "url": "/static/media/mission.6de143c4.svg"
+  },
+  {
+    "revision": "010a60cceb6b81608152c8e3935f8972",
+    "url": "/static/media/officers-background.010a60cc.jpg"
+  },
+  {
+    "revision": "19c4b015d5b2dfd6b2f20e129aa346fd",
+    "url": "/static/media/officers.19c4b015.svg"
+  },
+  {
+    "revision": "de433289cbc1f92da68deca2063106c9",
+    "url": "/static/media/social-background.de433289.jpg"
+  },
+  {
+    "revision": "74af54ff9a3d156eaa1f86bfb350d641",
+    "url": "/static/media/social-media.74af54ff.svg"
+  },
+  {
+    "revision": "a51986568a009029fee46216d5622473",
+    "url": "/static/media/sponsors.a5198656.svg"
+  }
+]);
--- scripts/sponsor.js
+++
@@ -1,3 +0,0 @@
-$('#return-top').click(function() {
-	window.scrollTo(0, 0);
-});
