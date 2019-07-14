b4e60e07d245d1c94e68cd43b91ce0f65f85d5fc
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-07-13 22:36:22 -0700

    fix: Form component not rendering correctly
    This was caused by a function being declared in render, which
    React cannot render into a component.

--- src/pages/home/Form.jsx
+++ src/pages/home/Form.jsx
@@ -42,94 +42,83 @@ const Signup = () => {
 					});
 				}}
 			>
-				{({handleSubmit, errors}) => (
-					<div id="mc_embed_signup">
-						<form
-							id="mc-embedded-subscribe-form"
-							name="mc-embedded-subscribe-form"
-							className="validate"
-							target="_blank"
-							noValidate
-							onSubmit={handleSubmit}
-						>
-							<div id="mc_embed_signup_scroll">
-								<h2>Subscribe to Our Events Newsletter</h2>
-								<div className="indicates-required">
-									<span className="asterisk">*</span> required
-								</div>
-								<div className="mc-field-group">
-									<label htmlFor="mce-EMAIL">
-										Email
-										<span className="asterisk">*</span>
-									</label>
-									<Field type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder="Email" />
-								</div>
-								<div className="mc-field-group">
-									<label htmlFor="mce-FNAME">
-										First Name
-										<span className="asterisk">*</span>
-									</label>
-									<Field type="text" name="FNAME" className="required" id="mce-FNAME" placeholder="e.g. Anish" />
-								</div>
-								<div className="mc-field-group">
-									<label htmlFor="mce-LNAME">
-										Last Name
-										<span className="asterisk">*</span>
-									</label>
-									<Field type="text" name="LNAME" className="required" id="mce-LNAME" placeholder="Last Name" />
-								</div>
-								{/* real people should not fill this in and expect good things - do not remove this or risk form bot signups 
+				{({handleSubmit, errors}) => {
+					const decoratedErrors = errors && {...errors};
+					Object.keys(decoratedErrors).forEach((key) => {
+						decoratedErrors[key] = (
+							<div key={key} className="response">
+								{decoratedErrors[key]}
+							</div>
+						);
+					});
+
+					return (
+						<div id="mc_embed_signup">
+							<form
+								id="mc-embedded-subscribe-form"
+								name="mc-embedded-subscribe-form"
+								className="validate"
+								target="_blank"
+								noValidate
+								onSubmit={handleSubmit}
+							>
+								<div id="mc_embed_signup_scroll">
+									<h2>Subscribe to Our Events Newsletter</h2>
+									<div className="indicates-required">
+										<span className="asterisk">*</span> required
+									</div>
+									<div className="mc-field-group">
+										<label htmlFor="mce-EMAIL">
+											Email
+											<span className="asterisk">*</span>
+										</label>
+										<Field type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder="Email" />
+									</div>
+									<div className="mc-field-group">
+										<label htmlFor="mce-FNAME">
+											First Name
+											<span className="asterisk">*</span>
+										</label>
+										<Field type="text" name="FNAME" className="required" id="mce-FNAME" placeholder="e.g. Anish" />
+									</div>
+									<div className="mc-field-group">
+										<label htmlFor="mce-LNAME">
+											Last Name
+											<span className="asterisk">*</span>
+										</label>
+										<Field type="text" name="LNAME" className="required" id="mce-LNAME" placeholder="Last Name" />
+									</div>
+									{/* real people should not fill this in and expect good things - do not remove this or risk form bot signups 
 								----------------
 								*/}
-								<div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
-									<input type="text" name="b_8773dffc3f528b0b18fd7aef8_11b31196c4" tabIndex="-1" defaultValue="" />
+									<div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
+										<input type="text" name="b_8773dffc3f528b0b18fd7aef8_11b31196c4" tabIndex="-1" defaultValue="" />
+									</div>
+									<div className="clear">
+										<input
+											type="submit"
+											value="Subscribe"
+											name="subscribe"
+											id="mc-embedded-subscribe"
+											className="button"
+										/>
+									</div>
+									<div id="mce-responses" className="clear">
+										<div className="mc-field-group">
+											<label htmlFor="mce-EMAIL">
+												Email
+												<span className="asterisk">*</span>
+											</label>
+											<Field type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder="Email" />
+											{errors && decoratedErrors['EMAIL']}
+										</div>
+										{hasSubmitted && <div className="response">Form has been submitted!</div>}
+									</div>
 								</div>
-								<div className="clear">
-									<input
-										type="submit"
-										value="Subscribe"
-										name="subscribe"
-										id="mc-embedded-subscribe"
-										className="button"
-									/>
-								</div>
-								<div id="mce-responses" className="clear">
-									{/* Brady fix implementation testing */}
-									{({handleSubmit, errors}) => {
-										const decoratedErrors = errors && {...errors};
-										Object.keys(decoratedErrors).forEach((key) => {
-											decoratedErrors[key] = (
-												<div key={key} className="response">
-													{decoratedErrors[key]}
-												</div>
-											);
-										});
-										return (
-											///....
-											<div className="mc-field-group">
-												<label htmlFor="mce-EMAIL">
-													Email
-													<span className="asterisk">*</span>
-												</label>
-												<Field
-													type="email"
-													name="EMAIL"
-													className="required email"
-													id="mce-EMAIL"
-													placeholder="Email"
-												/>
-												{errors && decoratedErrors['EMAIL']}
-											</div>
-										);
-									}
-									// -------------------------------------------
-									}
-									{hasSubmitted && <div className="response">Form has been submitted!</div>}
-								</div>
-							</div>
-						</form>
-					</div>
-				)}
+							</form>
+						</div>
+					);
+				}}
 			</Formik>
 		</div>
 	);
