c3e2e95b1c340a5762066ba6384c0e30fe30390f
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-08-11 00:41:34 -0700

    test(Routes): Migrate integration test to Routes tests
    Wrapping Sponsors with my own implementation of the Routes
    component felt like it was testing an implementation detail,
    so moving it to the Routes file removes the need for that.

--- src/Routes.test.jsx
+++ src/Routes.test.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render, fireEvent} from '@testing-library/react';
+
 import Routes from './Routes';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<Routes />, div);
-	ReactDOM.unmountComponentAtNode(div);
+describe('UWB ACM Website', () => {
+	describe('Sponsors Page', () => {
+		describe('Seed Sponsorships Section', () => {
+			it('should render a title for the section', () => {
+				const {getByText} = render(<Routes />);
+
+				// Navigate to sponsors
+				const SponsorButton = getByText('Sponsor');
+
+				expect(SponsorButton).toBeInTheDocument();
+
+				fireEvent.click(SponsorButton);
+
+				expect(getByText('Seed Sponsorships')).toBeInTheDocument();
+			});
+		});
+	});
 });
