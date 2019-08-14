fdda9b5ca5a5ce744ce8dd8232aa98acf1b1ba82
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-08-13 23:04:23 -0700

    refactor(testing): move test file to its own directory

--- src/__tests__/App.jsx
+++ src/__tests__/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
 
-import App from './App';
+import App from '../App';
 
 describe('UWB ACM Website', () => {
 	describe('Sponsors Page', () => {
