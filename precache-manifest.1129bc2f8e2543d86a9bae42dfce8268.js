3cc293b2066b63df97bd1ebc8031e3be0c9aa835
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-17 22:10:59 +0000

    Deploy UWB-ACM/uwbacm-site to github.com/UWB-ACM/uwbacm-site.git:gh-pages

---
+++ precache-manifest.1129bc2f8e2543d86a9bae42dfce8268.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9606aaaa7c4782e0623f806b98b093",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee87d66fa993d5441818",
+    "url": "/static/css/main.fdec2786.chunk.css"
+  },
+  {
+    "revision": "9780a0c50d7c60270146",
+    "url": "/static/js/2.d3db72ea.chunk.js"
+  },
+  {
+    "revision": "5fe0ea359c15f5fb08b4f9b30f28261d",
+    "url": "/static/js/2.d3db72ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee87d66fa993d5441818",
+    "url": "/static/js/main.b6e0074b.chunk.js"
+  },
+  {
+    "revision": "dc23d1f8794498371ae3",
+    "url": "/static/js/runtime-main.3e7e4044.js"
+  },
+  {
+    "revision": "bb4772e37e30c0a34775d81b2cea4de4",
+    "url": "/static/media/acm-large-logo.bb4772e3.jpg"
+  },
+  {
+    "revision": "a0e8cce8478d459264f45ee24fbe76c3",
+    "url": "/static/media/acm-white-transparent.a0e8cce8.svg"
+  },
+  {
+    "revision": "8d8693eb0136da08e2b0eb9c3c10ff68",
+    "url": "/static/media/avi-alex.8d8693eb.jpg"
+  },
+  {
+    "revision": "dc918dfa5ed94e3b27a72e7d0d9c407d",
+    "url": "/static/media/avi-annie.dc918dfa.jpg"
+  },
+  {
+    "revision": "017957f83adc2a6938e654fd5e3647da",
+    "url": "/static/media/avi-belle.017957f8.jpg"
+  },
+  {
+    "revision": "0a78f64018db9dfe90ffdce37e1da039",
+    "url": "/static/media/avi-billy.0a78f640.jpg"
+  },
+  {
+    "revision": "3c3ce14a26a3a484e93f7b12bcdcae45",
+    "url": "/static/media/avi-delaney.3c3ce14a.jpg"
+  },
+  {
+    "revision": "a078711fb553c2c868bde038f37edbf0",
+    "url": "/static/media/avi-drew.a078711f.jpg"
+  },
+  {
+    "revision": "1fa83d640e1f6dd60bf557c3e4615a14",
+    "url": "/static/media/avi-gi.1fa83d64.jpg"
+  },
+  {
+    "revision": "182258b43a786ee91f676b032391dd41",
+    "url": "/static/media/avi-hannah.182258b4.jpg"
+  },
+  {
+    "revision": "095eff09a3194cc8ad39c8c8d902c212",
+    "url": "/static/media/avi-jacob.095eff09.jpg"
+  },
+  {
+    "revision": "7a3d91d1030e709506437d151c7ef5f3",
+    "url": "/static/media/avi-katie.7a3d91d1.jpg"
+  },
+  {
+    "revision": "9e6353b80adf13d38f46c1e2b03eb3ee",
+    "url": "/static/media/avi-lizzy.9e6353b8.jpg"
+  },
+  {
+    "revision": "5e8decbef8b21d00e1fce1add67d42c8",
+    "url": "/static/media/avi-michele.5e8decbe.jpg"
+  },
+  {
+    "revision": "e53ab58e258b4eab5f35384c68505252",
+    "url": "/static/media/avi-pat.e53ab58e.jpg"
+  },
+  {
+    "revision": "4d6375b7cfae4ae7ca80411603dea0d8",
+    "url": "/static/media/avi-prat.4d6375b7.jpg"
+  },
+  {
+    "revision": "3d7b2ae20df391ea62d7c4c6e0465987",
+    "url": "/static/media/avi-tom.3d7b2ae2.jpg"
+  },
+  {
+    "revision": "5b9ee9cacc29945edacda28dade1e2d5",
+    "url": "/static/media/avi-zach.5b9ee9ca.jpg"
+  },
+  {
+    "revision": "4fadaf523dbe07c4599e618197bf9bfd",
+    "url": "/static/media/check.4fadaf52.svg"
+  },
+  {
+    "revision": "ca147c586330d253e607c505af7d7f76",
+    "url": "/static/media/contact-background.ca147c58.svg"
+  },
+  {
+    "revision": "ba31d4dd5156cbb466a158af3d653d82",
+    "url": "/static/media/contact.ba31d4dd.svg"
+  },
+  {
+    "revision": "1ee4bd6d8746fd04035602fed9349343",
+    "url": "/static/media/events.1ee4bd6d.svg"
+  },
+  {
+    "revision": "593e2633472485bf8cc0ab88cfe7e446",
+    "url": "/static/media/favicon.593e2633.ico"
+  },
+  {
+    "revision": "708c5a7aafcc9064e14e759c586e78c3",
+    "url": "/static/media/github-social.708c5a7a.svg"
+  },
+  {
+    "revision": "b713c4c926aa3d52d2301b86aee4efe8",
+    "url": "/static/media/hackathon_1.b713c4c9.jpg"
+  },
+  {
+    "revision": "b8b325af6a18a9c1464d2a63dc537908",
+    "url": "/static/media/hackathon_2.b8b325af.jpg"
+  },
+  {
+    "revision": "b9ace6f7404f7793d2de11b68b46565c",
+    "url": "/static/media/hackathon_3.b9ace6f7.jpg"
+  },
+  {
+    "revision": "bbd4ed28c6a76fe2a20410d4fec59796",
+    "url": "/static/media/hackathon_4.bbd4ed28.jpg"
+  },
+  {
+    "revision": "f136844eee0d899d2d4220b89f392f65",
+    "url": "/static/media/hackathon_5.f136844e.jpg"
+  },
+  {
+    "revision": "d48acce06d0eee04a00dc35f6608fb81",
+    "url": "/static/media/hackathon_6.d48acce0.jpg"
+  },
+  {
+    "revision": "b1d39dc66960e97a1387a682d5b2395e",
+    "url": "/static/media/home-background.b1d39dc6.jpg"
+  },
+  {
+    "revision": "f3db34660241502dccc64ee580c18691",
+    "url": "/static/media/join-acm.f3db3466.svg"
+  },
+  {
+    "revision": "cf9e3c80eb9d1f1424f80f40ec28c3ad",
+    "url": "/static/media/linkedin-social.cf9e3c80.svg"
+  },
+  {
+    "revision": "018b51194bda346a0b6afe601e1a4f14",
+    "url": "/static/media/lizzy.018b5119.jpg"
+  },
+  {
+    "revision": "502123dc5db89331fbdd7da8a4d654e0",
+    "url": "/static/media/mission.502123dc.svg"
+  },
+  {
+    "revision": "17c8b715fef0b7eb37894aedef08b5bf",
+    "url": "/static/media/officers-background.17c8b715.jpg"
+  },
+  {
+    "revision": "5a859cb438d89eafdd760b2ee3b74d49",
+    "url": "/static/media/officers.5a859cb4.svg"
+  },
+  {
+    "revision": "e56c8e30d07f4a5a9cd8115e887d97cd",
+    "url": "/static/media/pat.e56c8e30.jpg"
+  },
+  {
+    "revision": "de433289cbc1f92da68deca2063106c9",
+    "url": "/static/media/social-background.de433289.jpg"
+  },
+  {
+    "revision": "75c925c1134a5136de5ed7039bd768e6",
+    "url": "/static/media/social-media.75c925c1.svg"
+  },
+  {
+    "revision": "554e2c998a30b061f1df06788768781e",
+    "url": "/static/media/sponsors.554e2c99.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98da6ca4e0f37ea04a7eeec395bac901.js"
+  "/precache-manifest.1129bc2f8e2543d86a9bae42dfce8268.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
