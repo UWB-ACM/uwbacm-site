b7d642b6ac36a38e035cf7e74f301c005039c2c3
Ryan Russell <ryan.russell2000@gmail.com>
2020-08-06 16:43:51 -0700

    sss

---
+++ scripts/form_handler.js
@@ -0,0 +1,32 @@
+// // Handles newsletter submission request
+
+// function newsletterSignup() {
+//     console.log("Newsleter signup");
+// }
+
+
+// $(function () {
+//     $('form').submit(function () {
+//         // window.history.back();
+//         window.onbeforeunload = confirmExit;
+//         function confirmExit() {
+//             return "You have attempted to leave this page. Are you sure?";
+//         }
+//     });
+// })
+
+
+
+
+// // $("#signup-form").submit(function (e) {
+// //     console.log("Form Submitted");
+// //     // var mailchimp_name = document.getElementById("inputName").val;
+// //     var mailchimp_email = document.getElementById("mce-EMAIL").val;
+// //     $.post('https://uwbacm.us16.list-manage.com/subscribe/post?u=8773dffc3f528b0b18fd7aef8&amp;id=7a3e5f69db', {
+// //         // 'mailchimp-name': mailchimp_name,
+// //         'mailchimp-email': mailchimp_email
+// //     }).success(function (data) {
+// //         // do stuff on return
+// //     });
+// //     e.preventDefault()
+// // });
